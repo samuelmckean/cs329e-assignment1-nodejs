52d7c87219b183933cf327ea47a8a0be6b2a4d20
samuelmckean <samuel.mckean@gmail.com>
2020-02-06 18:45:01 -0600

    changed from XML to JSON

--- app.js
+++ app.js
@@ -4,9 +4,23 @@ const https = require('https');
 const port = 3000;
 
 app.get('/', (req, res) => {
-  https.get('https://raw.githubusercontent.com/devdattakulkarni/elements-of-web-programming/master/data/austin-pool-timings.xml', (res) => {
-    console.log('statusCode:', res.statusCode);
-    console.log('headers:', res.headers);
+  let poolData; // declare variable to store parsed data
+  // send get request to get pool data
+  https.get('https://data.austintexas.gov/api/views/jfqh-bqzu/rows.json?accessType=DOWNLOAD', (res) => {
+    let rawData = ''; // initialize empty string to store JSON data
+    res.on('data', (packet) => {
+      // process.stdout.write(packet);
+      rawData += packet;  // add each JSON packet to rawData 
+    }).on('end', (data) => {
+      try {
+        poolData = JSON.parse(rawData);
+        console.log(poolData);
+      } catch(err) {
+        console.error(`Got error: ${err.message}`);
+      }
+    });
+  }).on('error', (err) => {
+    console.error(`Got error: ${err.message}`);
   });
   res.sendStatus(200);
 });
